0c835526122197aa3a655c16329e1d2c528d20eb
Leslie Russell <leslierussell@gmail.com>
2026-05-27 15:06:49 -0500

    feat(math/stats): sample + population variance/stdev (zepo-7uu)

--- lib/math/stats.lisp
+++ lib/math/stats.lisp
@@ -1,5 +1,5 @@
 (module math/stats
-  (export ->vec sum mean)
+  (export ->vec sum mean variance stdev pvariance pstdev)
 
   (import math/core)    ; square, abs-close? (used later)
   (import math/linear)  ; solve, matvec, mat (used by ols)
@@ -15,4 +15,24 @@
   (define (mean xs)
     (let* ((v (->vec xs)) (n (vector-length v)))
       (if (= n 0) (error "mean: empty sequence"))
-      (/ (sum v) n))))
+      (/ (sum v) n)))
+
+  ;; zepo-7uu: two-pass sum of squared deviations from the mean.
+  (define (ss xs)
+    (let* ((v (->vec xs)) (n (vector-length v)) (m (mean v)))
+      (let loop ((i 0) (acc 0))
+        (if (= i n) acc
+            (loop (+ i 1) (+ acc (square (- (vector-ref v i) m))))))))
+
+  (define (pvariance xs)
+    (let* ((v (->vec xs)) (n (vector-length v)))
+      (if (= n 0) (error "pvariance: empty sequence"))
+      (/ (ss v) n)))
+
+  (define (variance xs)
+    (let* ((v (->vec xs)) (n (vector-length v)))
+      (if (< n 2) (error "variance: needs >= 2 values"))
+      (/ (ss v) (- n 1))))
+
+  (define (pstdev xs) (sqrt (pvariance xs)))
+  (define (stdev  xs) (sqrt (variance  xs))))
--- tests/math/stats_test.lisp
+++ tests/math/stats_test.lisp
@@ -8,4 +8,12 @@
   (is (abs-close? (mean (vector 1 2 3 4)) 2.5 1e-9))
   (is (abs-close? (mean '(2 4 6)) 4.0 1e-9)))
 
+(deftest stats/variance-stdev
+  ;; sample variance of (vector 2 4 4 4 5 5 7 9) = 4.571428571...
+  (is (abs-close? (variance (vector 2 4 4 4 5 5 7 9)) 4.5714285714 1e-9))
+  (is (abs-close? (stdev    (vector 2 4 4 4 5 5 7 9)) 2.1380899353 1e-9))
+  ;; population variance of same data = 4.0, pstdev = 2.0
+  (is (abs-close? (pvariance (vector 2 4 4 4 5 5 7 9)) 4.0 1e-9))
+  (is (abs-close? (pstdev    (vector 2 4 4 4 5 5 7 9)) 2.0 1e-9)))
+
 (run-tests)
